625be78d703fd091bc6a3d42ce54eafdefdf21a2
luisfernado28 <luisfer_2810@outlook.com>
2020-09-11 17:50:45 -0400

    Post and GET all users work

--- eloyahoraAPI/api_basic/serializers.py
+++ eloyahoraAPI/api_basic/serializers.py
@@ -1,30 +1,21 @@
 from rest_framework import serializers
-from .models import User
+from .models import *
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','firstname','lastname','mail','typeofuser','cellphone','direction','contact']
 
-    firstname = serializers.CharField(max_length=200)
-    lastname = serializers.CharField(max_length=200)
-    mail = serializers.EmailField()
-    typeofuser = serializers.IntegerField( )
-    cellphone = serializers.CharField(max_length=15)
-    direction = serializers.CharField(max_length=300)
-    contact = serializers.CharField(max_length=300)
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id','name']
 
-    def create(self, validated_data):
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','nameofproduct','weight','dimentions','color','brand','owner','tags','description']
 
-        return User.objects.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.firstname = validated_data.get('firstname',instance.firstname)
-        instance.lastname = validated_data.get('lastname',instance.lastname)
-        instance.mail = validated_data.get('mail',instance.mail)
-        instance.typeofuser = validated_data.get('typeofuser',instance.typeofuser)
-        instance.cellphone =validated_data.get('cellphone',instance.cellphone)
-        instance.direction = validated_data.get('direction',instance.direction)
-        instance.contact = validated_data.get('contact',instance.contact)
-        instance.save()
-        return instance
 
 
 
--- eloyahoraAPI/api_basic/views.py
+++ eloyahoraAPI/api_basic/views.py
@@ -1,3 +1,27 @@
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import *
+from .serializers import *
+
+from django.views.decorators.csrf import csrf_exempt
+
 from django.shortcuts import render
 
 # Create your views here.
+
+@csrf_exempt
+def user_list(request):
+
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer= UserSerializer(users, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data= JSONParser().parse(request)
+        serializer= UserSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors,status=400)
