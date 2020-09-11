a4ef20d57aff73ec79af4e7da74fb31ef490b1c2
luisfernado28 <luisfer_2810@outlook.com>
2020-09-11 16:37:34 -0400

    Model User Serialized New branch to be created

--- eloyahoraAPI/api_basic/serializers.py
+++ eloyahoraAPI/api_basic/serializers.py
@@ -1,19 +1,32 @@
 from rest_framework import serializers
-from .models import *
+from .models import User
 
-class userSerializer(serializers.Serializer):
-    typeuser = (
-        (1, 'Vendedor'),
-        (2, 'Comprador'),
-    )
-    firstname = serializers.CharField(max_length=200, null=False)
-    lastname = serializers.CharField(max_length=200, null=False)
-    mail = serializers.EmailField(null=False)
-    typeofuser = serializers.IntegerField(choices=typeuser, null=False)
-    cellphone = serializers.CharField(max_length=15, null=False)
+class UserSerializer(serializers.Serializer):
+
+    firstname = serializers.CharField(max_length=200)
+    lastname = serializers.CharField(max_length=200)
+    mail = serializers.EmailField()
+    typeofuser = serializers.IntegerField( )
+    cellphone = serializers.CharField(max_length=15)
     direction = serializers.CharField(max_length=300)
     contact = serializers.CharField(max_length=300)
 
     def create(self, validated_data):
+
         return User.objects.create(validated_data)
+
+    def update(self, instance, validated_data):
+        instance.firstname = validated_data.get('firstname',instance.firstname)
+        instance.lastname = validated_data.get('lastname',instance.lastname)
+        instance.mail = validated_data.get('mail',instance.mail)
+        instance.typeofuser = validated_data.get('typeofuser',instance.typeofuser)
+        instance.cellphone =validated_data.get('cellphone',instance.cellphone)
+        instance.direction = validated_data.get('direction',instance.direction)
+        instance.contact = validated_data.get('contact',instance.contact)
+        instance.save()
+        return instance
+
+
+
+
 
